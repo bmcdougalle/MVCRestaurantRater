9c9ce3b564f03345aaff6925ffe51faac5136c6f
bmcdougalle <bmcdougalle@gmail.com>
2021-03-22 19:59:12 -0400

    Initial Commit

---
+++ MVCRestaurantRater/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCRestaurantRater.MvcApplication" Language="C#" %>
---
+++ MVCRestaurantRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCRestaurantRater.Startup))]
+namespace MVCRestaurantRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
