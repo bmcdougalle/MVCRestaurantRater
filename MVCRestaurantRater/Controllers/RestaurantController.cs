3c7aa5361cbd86d0d9ca8f787b79abfd906308ec
bmcdougalle <bmcdougalle@gmail.com>
2021-03-23 19:29:14 -0400

    Added Edit and Delete methods to controller and To the Restaurant Views

--- MVCRestaurantRater/Controllers/RestaurantController.cs
+++ MVCRestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using MVCRestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -61,6 +62,40 @@ namespace MVCRestaurantRater.Controllers
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //Get Restaurant/Edit/id
+        //Get an id from the user
+        // Handle if the id is null
+        // Find a Restaurant by that Id
+        // if the restaurant doesn't exist
+        // Return the restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+        }
+
+        //Post: Restaurant/Edit/Id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
